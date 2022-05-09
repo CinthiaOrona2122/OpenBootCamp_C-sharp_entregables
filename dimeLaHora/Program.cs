7864bc49eb3ab715c3989b10f48bd6808494738d
Cinthia M. Orona <devrider2021@gmail.com>
2022-05-08 21:09:01 -0300

    Entrega del Ejercicio 1

---
+++ dimeLaHora/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+DateTime fechaConHora = new DateTime(2022, 5, 8, 20, 52, 00);
+
+System.Console.WriteLine(fechaConHora.ToString("dd/mm/yy hh:mm:ss"));
+
+Console.ReadLine();
+Console.ReadKey();
